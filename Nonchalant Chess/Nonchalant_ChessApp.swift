ab96caf31a28de0477fddd54b8bb5286a6a1c774
Divyansh Bhardwaj <divyansh.bhardwaj@bobble.ai>
2025-02-11 22:24:02 +0530

    Initial Commit

---
+++ Nonchalant Chess/Nonchalant_ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nonchalant_ChessApp.swift
+//  Nonchalant Chess
+//
+//  Created by Divyansh Bhardwaj on 11/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Nonchalant_ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
